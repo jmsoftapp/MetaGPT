f04ef511bc4d207c9d5e53f7e125f6d111a2db18
jmcsw <jmcsw@163.com>
2024-04-25 22:25:33 +0800

    20240425010

---
+++ mycode/test1.py
@@ -0,0 +1,27 @@
+import asyncio
+from metagpt.roles import (
+    Architect,
+    Engineer,
+    ProductManager,
+    ProjectManager,
+)
+from metagpt.team import Team
+
+async def startup(idea: str):
+    company = Team()
+    company.hire(
+        [
+            ProductManager(),
+            Architect(),
+            ProjectManager(),
+            Engineer(),
+        ]
+    )
+    company.invest(investment=3.0)  # 提供3美元的资金，如果超出就停止
+    company.run_project(idea=idea)
+
+    await company.run(n_round=5)  # 这个项目跑5轮
+
+if __name__ == "__main__":
+    asyncio.run(startup(idea="Write a program that automatically generates an intelligent agent Action, which is inherited from the Action class of the multi-agent framework MetaGPT"))  
+
